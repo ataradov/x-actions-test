d1f3b5dde3e5c701c3ee6d1f2b68f84e2171bdb8
Alex Taradov <alex@taradov.com>
2021-03-28 12:47:26 -0700

    test

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-  printf("CMSIS-DAP SWD programmer built " __DATE__ " " __TIME__ " \n\n", VERSION);
+  printf("CMSIS-DAP SWD programmer built " __DATE__ " " __TIME__ " \n\n");
 
   return 0;
 }
